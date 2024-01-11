ee61cbc55a07af3d3cabf759ed2599c2468a20c6
achiu5 <67072108+achiu5@users.noreply.github.com>
2024-01-10 18:21:13 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		Bracket myBracket = new Bracket();
+		
+		//myBracket.printBracket();
+		myBracket.randomBracket();
+		myBracket.printBracket();
+		
+		myBracket.startBracket();
+	}
+}
